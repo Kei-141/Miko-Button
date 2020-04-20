f7dc8f9eb96a74e35ef105cff22275893cb70fba
Kei <ruler141@gmail.com>
2020-04-20 22:09:59 +0900

    commit

--- Miko Button/Form1.Designer.cs
+++ Miko Button/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Miko_Button
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,20 +28,398 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.hello = new System.Windows.Forms.Button();
+            this.nyahello = new System.Windows.Forms.Button();
+            this.faq = new System.Windows.Forms.Button();
+            this.shiketennile = new System.Windows.Forms.Button();
+            this.orala = new System.Windows.Forms.Button();
+            this.nile = new System.Windows.Forms.Button();
+            this.mikohawarukunai = new System.Windows.Forms.Button();
+            this.shike = new System.Windows.Forms.Button();
+            this.tennile = new System.Windows.Forms.Button();
+            this.unchi = new System.Windows.Forms.Button();
+            this.oioioi = new System.Windows.Forms.Button();
+            this.kusa = new System.Windows.Forms.Button();
+            this.nainatta = new System.Windows.Forms.Button();
+            this.hajimaruyo = new System.Windows.Forms.Button();
+            this.oitan = new System.Windows.Forms.Button();
+            this.ababababa = new System.Windows.Forms.Button();
+            this.twuttwu = new System.Windows.Forms.Button();
+            this.altutyu = new System.Windows.Forms.Button();
+            this.wauwau = new System.Windows.Forms.Button();
+            this.dwunchi = new System.Windows.Forms.Button();
+            this.dondonpa = new System.Windows.Forms.Button();
+            this.faaaaa = new System.Windows.Forms.Button();
+            this.da = new System.Windows.Forms.Button();
+            this.konnanottenaiyo = new System.Windows.Forms.Button();
+            this.tuntun = new System.Windows.Forms.Button();
+            this.bababababa = new System.Windows.Forms.Button();
+            this.babababa = new System.Windows.Forms.Button();
+            this.hohyabupa = new System.Windows.Forms.Button();
+            this.ahin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // hello
+            // 
+            this.hello.Location = new System.Drawing.Point(12, 12);
+            this.hello.Name = "hello";
+            this.hello.Size = new System.Drawing.Size(83, 60);
+            this.hello.TabIndex = 0;
+            this.hello.Text = "はろ～";
+            this.hello.UseVisualStyleBackColor = true;
+            this.hello.Click += new System.EventHandler(this.hello_Click);
+            // 
+            // nyahello
+            // 
+            this.nyahello.Location = new System.Drawing.Point(101, 12);
+            this.nyahello.Name = "nyahello";
+            this.nyahello.Size = new System.Drawing.Size(83, 60);
+            this.nyahello.TabIndex = 1;
+            this.nyahello.Text = "にゃっはろ～";
+            this.nyahello.UseVisualStyleBackColor = true;
+            this.nyahello.Click += new System.EventHandler(this.nyahello_Click);
+            // 
+            // faq
+            // 
+            this.faq.Location = new System.Drawing.Point(190, 12);
+            this.faq.Name = "faq";
+            this.faq.Size = new System.Drawing.Size(83, 60);
+            this.faq.TabIndex = 2;
+            this.faq.Text = "FAQ";
+            this.faq.UseVisualStyleBackColor = true;
+            this.faq.Click += new System.EventHandler(this.faq_Click);
+            // 
+            // shiketennile
+            // 
+            this.shiketennile.Location = new System.Drawing.Point(279, 12);
+            this.shiketennile.Name = "shiketennile";
+            this.shiketennile.Size = new System.Drawing.Size(83, 60);
+            this.shiketennile.TabIndex = 3;
+            this.shiketennile.Text = "しけてんにぇ";
+            this.shiketennile.UseVisualStyleBackColor = true;
+            this.shiketennile.Click += new System.EventHandler(this.shiketennile_Click);
+            // 
+            // orala
+            // 
+            this.orala.Location = new System.Drawing.Point(368, 12);
+            this.orala.Name = "orala";
+            this.orala.Size = new System.Drawing.Size(83, 60);
+            this.orala.TabIndex = 4;
+            this.orala.Text = "オラァ！";
+            this.orala.UseVisualStyleBackColor = true;
+            this.orala.Click += new System.EventHandler(this.orala_Click);
+            // 
+            // nile
+            // 
+            this.nile.Location = new System.Drawing.Point(457, 12);
+            this.nile.Name = "nile";
+            this.nile.Size = new System.Drawing.Size(83, 60);
+            this.nile.TabIndex = 5;
+            this.nile.Text = "にぇ！";
+            this.nile.UseVisualStyleBackColor = true;
+            this.nile.Click += new System.EventHandler(this.nile_Click);
+            // 
+            // mikohawarukunai
+            // 
+            this.mikohawarukunai.Location = new System.Drawing.Point(12, 78);
+            this.mikohawarukunai.Name = "mikohawarukunai";
+            this.mikohawarukunai.Size = new System.Drawing.Size(83, 60);
+            this.mikohawarukunai.TabIndex = 6;
+            this.mikohawarukunai.Text = "みこは悪くないよねぇ？";
+            this.mikohawarukunai.UseVisualStyleBackColor = true;
+            this.mikohawarukunai.Click += new System.EventHandler(this.mikohawarukunai_Click);
+            // 
+            // shike
+            // 
+            this.shike.Location = new System.Drawing.Point(101, 78);
+            this.shike.Name = "shike";
+            this.shike.Size = new System.Drawing.Size(83, 60);
+            this.shike.TabIndex = 7;
+            this.shike.Text = "しけ";
+            this.shike.UseVisualStyleBackColor = true;
+            this.shike.Click += new System.EventHandler(this.shike_Click);
+            // 
+            // tennile
+            // 
+            this.tennile.Location = new System.Drawing.Point(190, 78);
+            this.tennile.Name = "tennile";
+            this.tennile.Size = new System.Drawing.Size(83, 60);
+            this.tennile.TabIndex = 8;
+            this.tennile.Text = "てんにぇ！";
+            this.tennile.UseVisualStyleBackColor = true;
+            this.tennile.Click += new System.EventHandler(this.tennile_Click);
+            // 
+            // unchi
+            // 
+            this.unchi.Location = new System.Drawing.Point(279, 78);
+            this.unchi.Name = "unchi";
+            this.unchi.Size = new System.Drawing.Size(83, 60);
+            this.unchi.TabIndex = 9;
+            this.unchi.Text = "うんちしました";
+            this.unchi.UseVisualStyleBackColor = true;
+            this.unchi.Click += new System.EventHandler(this.unchi_Click);
+            // 
+            // oioioi
+            // 
+            this.oioioi.Location = new System.Drawing.Point(368, 78);
+            this.oioioi.Name = "oioioi";
+            this.oioioi.Size = new System.Drawing.Size(83, 60);
+            this.oioioi.TabIndex = 10;
+            this.oioioi.Text = "おいおいおい";
+            this.oioioi.UseVisualStyleBackColor = true;
+            this.oioioi.Click += new System.EventHandler(this.oioioi_Click);
+            // 
+            // kusa
+            // 
+            this.kusa.Location = new System.Drawing.Point(457, 78);
+            this.kusa.Name = "kusa";
+            this.kusa.Size = new System.Drawing.Size(83, 60);
+            this.kusa.TabIndex = 11;
+            this.kusa.Text = "草";
+            this.kusa.UseVisualStyleBackColor = true;
+            this.kusa.Click += new System.EventHandler(this.kusa_Click);
+            // 
+            // nainatta
+            // 
+            this.nainatta.Location = new System.Drawing.Point(12, 144);
+            this.nainatta.Name = "nainatta";
+            this.nainatta.Size = new System.Drawing.Size(83, 60);
+            this.nainatta.TabIndex = 12;
+            this.nainatta.Text = "ないなった";
+            this.nainatta.UseVisualStyleBackColor = true;
+            this.nainatta.Click += new System.EventHandler(this.nainatta_Click);
+            // 
+            // hajimaruyo
+            // 
+            this.hajimaruyo.Location = new System.Drawing.Point(101, 144);
+            this.hajimaruyo.Name = "hajimaruyo";
+            this.hajimaruyo.Size = new System.Drawing.Size(83, 60);
+            this.hajimaruyo.TabIndex = 13;
+            this.hajimaruyo.Text = "はじまるよ～";
+            this.hajimaruyo.UseVisualStyleBackColor = true;
+            this.hajimaruyo.Click += new System.EventHandler(this.hajimaruyo_Click);
+            // 
+            // oitan
+            // 
+            this.oitan.Location = new System.Drawing.Point(190, 144);
+            this.oitan.Name = "oitan";
+            this.oitan.Size = new System.Drawing.Size(83, 60);
+            this.oitan.TabIndex = 14;
+            this.oitan.Text = "おいたん";
+            this.oitan.UseVisualStyleBackColor = true;
+            this.oitan.Click += new System.EventHandler(this.oitan_Click);
+            // 
+            // ababababa
+            // 
+            this.ababababa.Location = new System.Drawing.Point(279, 144);
+            this.ababababa.Name = "ababababa";
+            this.ababababa.Size = new System.Drawing.Size(83, 60);
+            this.ababababa.TabIndex = 15;
+            this.ababababa.Text = "あばばばばばば";
+            this.ababababa.UseVisualStyleBackColor = true;
+            this.ababababa.Click += new System.EventHandler(this.ababababa_Click);
+            // 
+            // twuttwu
+            // 
+            this.twuttwu.Location = new System.Drawing.Point(368, 144);
+            this.twuttwu.Name = "twuttwu";
+            this.twuttwu.Size = new System.Drawing.Size(83, 60);
+            this.twuttwu.TabIndex = 16;
+            this.twuttwu.Text = "とぅっとぅとぅっとぅとぅー";
+            this.twuttwu.UseVisualStyleBackColor = true;
+            this.twuttwu.Click += new System.EventHandler(this.twuttwu_Click);
+            // 
+            // altutyu
+            // 
+            this.altutyu.Location = new System.Drawing.Point(457, 144);
+            this.altutyu.Name = "altutyu";
+            this.altutyu.Size = new System.Drawing.Size(83, 60);
+            this.altutyu.TabIndex = 17;
+            this.altutyu.Text = "あっちゅ！あっちゅ！";
+            this.altutyu.UseVisualStyleBackColor = true;
+            this.altutyu.Click += new System.EventHandler(this.altutyu_Click);
+            // 
+            // wauwau
+            // 
+            this.wauwau.Location = new System.Drawing.Point(12, 210);
+            this.wauwau.Name = "wauwau";
+            this.wauwau.Size = new System.Drawing.Size(83, 60);
+            this.wauwau.TabIndex = 18;
+            this.wauwau.Text = "わーうわーうわーうわーうわーうわーう";
+            this.wauwau.UseVisualStyleBackColor = true;
+            this.wauwau.Click += new System.EventHandler(this.wauwau_Click);
+            // 
+            // dwunchi
+            // 
+            this.dwunchi.Location = new System.Drawing.Point(101, 210);
+            this.dwunchi.Name = "dwunchi";
+            this.dwunchi.Size = new System.Drawing.Size(83, 60);
+            this.dwunchi.TabIndex = 19;
+            this.dwunchi.Text = "どぅん！ち！どぅんどぅんち！";
+            this.dwunchi.UseVisualStyleBackColor = true;
+            this.dwunchi.Click += new System.EventHandler(this.dwunchi_Click);
+            // 
+            // dondonpa
+            // 
+            this.dondonpa.Location = new System.Drawing.Point(190, 210);
+            this.dondonpa.Name = "dondonpa";
+            this.dondonpa.Size = new System.Drawing.Size(83, 60);
+            this.dondonpa.TabIndex = 20;
+            this.dondonpa.Text = "どんどんぱ！";
+            this.dondonpa.UseVisualStyleBackColor = true;
+            this.dondonpa.Click += new System.EventHandler(this.dondonpa_Click);
+            // 
+            // faaaaa
+            // 
+            this.faaaaa.Location = new System.Drawing.Point(279, 210);
+            this.faaaaa.Name = "faaaaa";
+            this.faaaaa.Size = new System.Drawing.Size(83, 60);
+            this.faaaaa.TabIndex = 21;
+            this.faaaaa.Text = "ファーーーー";
+            this.faaaaa.UseVisualStyleBackColor = true;
+            this.faaaaa.Click += new System.EventHandler(this.faaaaa_Click);
+            // 
+            // da
+            // 
+            this.da.Location = new System.Drawing.Point(368, 210);
+            this.da.Name = "da";
+            this.da.Size = new System.Drawing.Size(83, 60);
+            this.da.TabIndex = 22;
+            this.da.Text = "ダ！";
+            this.da.UseVisualStyleBackColor = true;
+            this.da.Click += new System.EventHandler(this.da_Click);
+            // 
+            // konnanottenaiyo
+            // 
+            this.konnanottenaiyo.Location = new System.Drawing.Point(457, 210);
+            this.konnanottenaiyo.Name = "konnanottenaiyo";
+            this.konnanottenaiyo.Size = new System.Drawing.Size(83, 60);
+            this.konnanottenaiyo.TabIndex = 23;
+            this.konnanottenaiyo.Text = "こんなのってないよ";
+            this.konnanottenaiyo.UseVisualStyleBackColor = true;
+            this.konnanottenaiyo.Click += new System.EventHandler(this.konnanottenaiyo_Click);
+            // 
+            // tuntun
+            // 
+            this.tuntun.Location = new System.Drawing.Point(12, 276);
+            this.tuntun.Name = "tuntun";
+            this.tuntun.Size = new System.Drawing.Size(83, 60);
+            this.tuntun.TabIndex = 24;
+            this.tuntun.Text = "つんつん";
+            this.tuntun.UseVisualStyleBackColor = true;
+            this.tuntun.Click += new System.EventHandler(this.tuntun_Click);
+            // 
+            // bababababa
+            // 
+            this.bababababa.Location = new System.Drawing.Point(101, 276);
+            this.bababababa.Name = "bababababa";
+            this.bababababa.Size = new System.Drawing.Size(83, 60);
+            this.bababababa.TabIndex = 25;
+            this.bababababa.Text = "ばばばばば↑";
+            this.bababababa.UseVisualStyleBackColor = true;
+            this.bababababa.Click += new System.EventHandler(this.bababababa_Click);
+            // 
+            // babababa
+            // 
+            this.babababa.Location = new System.Drawing.Point(190, 276);
+            this.babababa.Name = "babababa";
+            this.babababa.Size = new System.Drawing.Size(83, 60);
+            this.babababa.TabIndex = 26;
+            this.babababa.Text = "ばばばばば↓";
+            this.babababa.UseVisualStyleBackColor = true;
+            this.babababa.Click += new System.EventHandler(this.babababa_Click);
+            // 
+            // hohyabupa
+            // 
+            this.hohyabupa.Location = new System.Drawing.Point(279, 276);
+            this.hohyabupa.Name = "hohyabupa";
+            this.hohyabupa.Size = new System.Drawing.Size(83, 60);
+            this.hohyabupa.TabIndex = 27;
+            this.hohyabupa.Text = "ほひゃぶぱぴぱぷぺぶばぶばぶ";
+            this.hohyabupa.UseVisualStyleBackColor = true;
+            this.hohyabupa.Click += new System.EventHandler(this.hohyabupa_Click);
+            // 
+            // ahin
+            // 
+            this.ahin.Location = new System.Drawing.Point(368, 276);
+            this.ahin.Name = "ahin";
+            this.ahin.Size = new System.Drawing.Size(83, 60);
+            this.ahin.TabIndex = 28;
+            this.ahin.Text = "あひん！あひん！";
+            this.ahin.UseVisualStyleBackColor = true;
+            this.ahin.Click += new System.EventHandler(this.ahin_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 553);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(551, 347);
+            this.Controls.Add(this.ahin);
+            this.Controls.Add(this.hohyabupa);
+            this.Controls.Add(this.babababa);
+            this.Controls.Add(this.bababababa);
+            this.Controls.Add(this.tuntun);
+            this.Controls.Add(this.konnanottenaiyo);
+            this.Controls.Add(this.da);
+            this.Controls.Add(this.faaaaa);
+            this.Controls.Add(this.dondonpa);
+            this.Controls.Add(this.dwunchi);
+            this.Controls.Add(this.wauwau);
+            this.Controls.Add(this.altutyu);
+            this.Controls.Add(this.twuttwu);
+            this.Controls.Add(this.ababababa);
+            this.Controls.Add(this.oitan);
+            this.Controls.Add(this.hajimaruyo);
+            this.Controls.Add(this.nainatta);
+            this.Controls.Add(this.kusa);
+            this.Controls.Add(this.oioioi);
+            this.Controls.Add(this.unchi);
+            this.Controls.Add(this.tennile);
+            this.Controls.Add(this.shike);
+            this.Controls.Add(this.mikohawarukunai);
+            this.Controls.Add(this.nile);
+            this.Controls.Add(this.orala);
+            this.Controls.Add(this.shiketennile);
+            this.Controls.Add(this.faq);
+            this.Controls.Add(this.nyahello);
+            this.Controls.Add(this.hello);
+            this.Name = "MainForm";
+            this.Text = "Miko Button";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button hello;
+        private System.Windows.Forms.Button nyahello;
+        private System.Windows.Forms.Button faq;
+        private System.Windows.Forms.Button shiketennile;
+        private System.Windows.Forms.Button orala;
+        private System.Windows.Forms.Button nile;
+        private System.Windows.Forms.Button mikohawarukunai;
+        private System.Windows.Forms.Button shike;
+        private System.Windows.Forms.Button tennile;
+        private System.Windows.Forms.Button unchi;
+        private System.Windows.Forms.Button oioioi;
+        private System.Windows.Forms.Button kusa;
+        private System.Windows.Forms.Button nainatta;
+        private System.Windows.Forms.Button hajimaruyo;
+        private System.Windows.Forms.Button oitan;
+        private System.Windows.Forms.Button ababababa;
+        private System.Windows.Forms.Button twuttwu;
+        private System.Windows.Forms.Button altutyu;
+        private System.Windows.Forms.Button wauwau;
+        private System.Windows.Forms.Button dwunchi;
+        private System.Windows.Forms.Button dondonpa;
+        private System.Windows.Forms.Button faaaaa;
+        private System.Windows.Forms.Button da;
+        private System.Windows.Forms.Button konnanottenaiyo;
+        private System.Windows.Forms.Button tuntun;
+        private System.Windows.Forms.Button bababababa;
+        private System.Windows.Forms.Button babababa;
+        private System.Windows.Forms.Button hohyabupa;
+        private System.Windows.Forms.Button ahin;
     }
 }
 
--- Miko Button/Form1.cs
+++ Miko Button/Form1.cs
@@ -7,14 +7,179 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Miko_Button
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private string current_dir = System.Environment.CurrentDirectory + "\\data\\";
+
+        private System.Media.SoundPlayer player = null;
+        private void StopSound()
+        {
+            if (player != null)
+            {
+                player.Stop();
+                player.Dispose();
+                player = null;
+            }
+        }
+
+        private void PlaySound(string sound_file)
+        {
+            player = new System.Media.SoundPlayer(sound_file);
+            player.Play();
+        }
+
+        private void hello_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + hello.Text + ".wav");
+        }
+
+        private void nyahello_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + nyahello.Text + ".wav");
+        }
+
+        private void faq_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + faq.Text + ".wav");
+        }
+
+        private void shiketennile_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + shiketennile.Text + ".wav");
+        }
+
+        private void orala_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + orala.Text + ".wav");
+        }
+
+        private void nile_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + nile.Text + ".wav");
+        }
+
+        private void mikohawarukunai_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + mikohawarukunai.Text + ".wav");
+        }
+
+        private void shike_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + shike.Text + ".wav");
+        }
+
+        private void tennile_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + tennile.Text + ".wav");
+        }
+
+        private void unchi_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + unchi.Text + ".wav");
+        }
+
+        private void oioioi_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + oioioi.Text + ".wav");
+        }
+
+        private void kusa_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + kusa.Text + ".wav");
+        }
+
+        private void nainatta_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + nainatta.Text + ".wav");
+        }
+
+        private void hajimaruyo_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + hajimaruyo.Text + ".wav");
+        }
+
+        private void oitan_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + oitan.Text + ".wav");
+        }
+
+        private void ababababa_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + ababababa.Text + ".wav");
+        }
+
+        private void twuttwu_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + twuttwu.Text + ".wav");
+        }
+
+        private void altutyu_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + altutyu.Text + ".wav");
+        }
+
+        private void wauwau_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + wauwau.Text + ".wav");
+        }
+
+        private void dwunchi_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + dwunchi.Text + ".wav");
+        }
+
+        private void dondonpa_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + dondonpa.Text + ".wav");
+        }
+
+        private void faaaaa_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + faaaaa.Text + ".wav");
+        }
+
+        private void da_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + da.Text + ".wav");
+        }
+
+        private void konnanottenaiyo_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + konnanottenaiyo.Text + ".wav");
+        }
+
+        private void tuntun_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + tuntun.Text + ".wav");
+        }
+
+        private void bababababa_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + bababababa.Text + ".wav");
+        }
+
+        private void babababa_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + babababa.Text + ".wav");
+        }
+
+        private void hohyabupa_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + hohyabupa.Text + ".wav");
+        }
+
+        private void ahin_Click(object sender, EventArgs e)
+        {
+            PlaySound(current_dir + ahin.Text + ".wav");
         }
     }
 }
